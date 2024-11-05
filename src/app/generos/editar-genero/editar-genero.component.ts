7ec5bdfe6f65779c8beca3faa4a36c63d69a1ebe
Maritza Perez <yamilethfunes098@gmail.com>
2024-11-05 13:25:40 -0600

    Formularios corregidos

--- src/app/generos/editar-genero/editar-genero.component.ts
+++ src/app/generos/editar-genero/editar-genero.component.ts
@@ -1,11 +1,9 @@
 import { Component, Input, numberAttribute } from '@angular/core';
-import { FormularioGeneroComponent } from "../formulario-genero/formulario-genero.component";
-import { GeneroCreacionDTO, GeneroDTO } from '../generos';
 
 @Component({
   selector: 'app-editar-genero',
   standalone: true,
-  imports: [FormularioGeneroComponent],
+  imports: [],
   templateUrl: './editar-genero.component.html',
   styleUrl: './editar-genero.component.css'
 })
@@ -13,11 +11,5 @@ export class EditarGeneroComponent {
   
   @Input({transform: numberAttribute})
   id!: number;
-
-  genero: GeneroDTO = {id: 1, nombre: 'Comedia'};
-
-  guardarCambios(genero: GeneroCreacionDTO){
-    console.log('editando el género', genero);
-  }
 
 }
