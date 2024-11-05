6829e288a3e3a36c1464efcf59feb43d4a2a53db
Maritza Perez <yamilethfunes098@gmail.com>
2024-11-05 12:33:29 -0600

    Formularios

--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -13,20 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ListadoPeliculasComponent {
 
-  @Input({required: true})
+  @Input({ required: true })
   peliculas!: any[];
-  
-  agregarPelicula(){
-    this.peliculas.push({
-      titulo: 'Inception',
-      fechaLanzamiento: new Date('2012-07-03'),
-      precio: 500
-    })
-  }
 
-  remover(pelicula: any){
-    const indice = this.peliculas.findIndex((peliculaActual: any) => peliculaActual.titulo === pelicula.titulo);
-    this.peliculas.splice(indice, 1);
-  }
 
 }
